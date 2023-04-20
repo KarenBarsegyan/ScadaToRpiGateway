2aa0f1022dbd0536966d54818af70b5334282103
KarenBarsegyan <karen.barsegyan-2001@bk.ru>
2023-04-20 16:44:21 +0300

    Lets go to rpi

--- ProgrammerClient.py
+++ ProgrammerClient.py
@@ -1,11 +1,20 @@
 import json
 import socket
 from ScadaDataTypes import ScadaData
+from PyQt6.QtCore import QThread, pyqtSignal
+import asyncio
+from async_timeout import timeout
 
-class ScadaClient:
-    def __init__(self, KU: int):
+class ScadaClient(QThread):
+    finished = pyqtSignal(bool)
+
+    def __init__(self, KU: int, parent=None):
+        QThread.__init__(self, parent)
+
+        self._messgae = []
+
         data_dict = {
-            "IMEI": "123456789",
+            "IMEI": "",
             "KU": KU,
             "SWVersion": "",
             "Result": True,
@@ -21,36 +30,44 @@ class ScadaClient:
     def SetFWVersion(self, SW):
         self._data_to_send.sim_data.SWVersion = SW
 
-    def Send(self, result):
+    def UpdateMessage(self, msg: str):
+        self._messgae.append(msg)
+
+    def SetResult(self, result: bool):
+        self._data_to_send.sim_data.Result = result
+
+    async def _send(self):
         self._data_to_send.SetEndTime()
 
-        if result == 'Success':
-            self._data_to_send.sim_data.Result = True
-        else:
-            self._data_to_send.sim_data.Result = False
-            self._data_to_send.sim_data.Message = result
+        self._data_to_send.sim_data.Message = str(self._messgae)
 
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-            try:
-                sock.connect(('192.168.88.246', 8080))
-                sock.send(self._data_to_send.GetDataInBytes())
+            result = False
+            for i in range(5):
+                try:
+                    sock.connect(('192.168.88.246', 8080))
+                    sock.send(self._data_to_send.GetDataInBytes())
 
-                data_to_recv = sock.recv(1024)
+                    data_to_recv = sock.recv(1024)
 
-                if data_to_recv == self._data_to_send.GetDataInBytes():
-                    print("Send \ recv succesfully")
-            except Exception as ex:
-                print(ex)
+                    scada_data_recv = ScadaData()
+                    scada_data_recv.SetDataFromBytes(data_to_recv)
 
-    def _SendBytes(self, data_to_send) -> ScadaData:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # create client socket
-        sock.connect(('127.0.0.1', 8080))  # connect to server socket
-        sock.send(data_to_send)  # send data
-        data = sock.recv(1024)  # read server answer
-        sock.close()  # close connection
-        return ScadaData(**json.loads(data)) # return Answer in ScadaData format
-    
+                    if scada_data_recv.GetDataInBytes() == self._data_to_send.GetDataInBytes():
+                        self.finished.emit(True)
+                        result = True
+                        break
+                except Exception as ex:
+                    print(ex)
+
+                await asyncio.sleep(1)
+
+            if not result:
+                self.finished.emit(False)
+
+    def run(self) -> None:
+        asyncio.run(self._send())    
 
 if __name__ == '__main__':
     cl = ScadaClient(1)
-    cl.Send('Success')
+    cl.run()
--- Workplace.py
+++ Workplace.py
@@ -19,13 +19,11 @@ class Workplace(QVBoxLayout):
         super().__init__()
         self._number_of_points = 0
         self._config = yaml.load(open("configuration.yml"), yaml.SafeLoader)
-        self.constIp = self._config['const_ip_part']
-        self.startIp = self._config['start_ip']
         self.usecase = self._config['use_case']
-        
         self._wpnumber = wp_number  # номер текущего рп
-        self._uiAddWidgets()  # заполнение рабочего пространства
+        self._scada_client = ScadaClient(self._wpnumber+1)
 
+        self._uiAddWidgets()  # заполнение рабочего пространства
 
     def _uiAddWidgets(self):
         self._uiAddLogField()
@@ -70,19 +68,34 @@ class Workplace(QVBoxLayout):
                 self._rpi_thread = WebSocketClient(f'sim7600prg{self._wpnumber+1}.local', 'Start')
                 self._rpi_thread.progress.connect(self._show_new_log)
                 self._rpi_thread.finished.connect(self._change_status)
-                self._rpi_thread.ping.connect(self._ping)
+                self._rpi_thread.ping.connect(self._ping_callback)
                 self._rpi_thread.start()
 
             if cmd == 'Ping':
                 self._rpi_thread = WebSocketClient(f'sim7600prg{self._wpnumber+1}.local', 'Ping')
-                self._rpi_thread.finished.connect(self._ping_ok)
+                self._rpi_thread.finished.connect(self._start_ping_callback)
                 self._rpi_thread.start()
 
-    def _ping_ok(self, work_result:bool):
+    def _scada_send(self, result: bool):
+        if self.usecase == 'scada':
+            self._scada_client.finished.connect(self._scada_sended_callback)
+            self._scada_client.SetResult(result)
+            self._scada_client.start()
+
+    def _scada_sended_callback(self, res: bool):
+        if res:
+            self._show_new_log('LogOk', 'Scada Received msg Ok')
+        else:
+            self._show_new_log('LogErr', 'Scada did not receive msg')
+
+    def _start_ping_callback(self, work_result:bool):
         if work_result:
             self._status.setText('Программатор готов')
             self._status.setStyleSheet('background-color: green')
             self._button.setFlat(False)
+
+            self._scada_client.UpdateMessage('Ready To Start')
+            self._scada_send(True)
         
     def _show_new_log(self, log_level: str, log_msg:str):
         item = QListWidgetItem(log_msg)
@@ -90,8 +103,10 @@ class Workplace(QVBoxLayout):
             item.setForeground(QColor('#7fc97f')) # green
         if log_level == 'LogWarn':
             item.setForeground(QColor('#ffff99')) # yellow
+            self._scada_client.UpdateMessage(log_msg)
         elif log_level == 'LogErr':
             item.setForeground(QColor('#f00000')) # red
+            self._scada_client.UpdateMessage(log_msg)
 
         if 'Start flashing' in log_msg:
             self._scada_client = ScadaClient(self._wpnumber+1)
@@ -104,12 +119,12 @@ class Workplace(QVBoxLayout):
 
     def _change_status(self, work_result:bool):
         if work_result:
-            self._scada_client.Send('Success')
+            self._scada_send(True)
 
             self._status.setText('Успешно')
             self._status.setStyleSheet('background-color: green')
         else:
-            self._scada_client.Send('Error')
+            self._scada_send(False)
 
             self._status.setText('Ошибка')
             self._status.setStyleSheet('background-color: red')
@@ -117,7 +132,7 @@ class Workplace(QVBoxLayout):
         self._button.setFlat(False)
         del(self._rpi_thread)
 
-    def _ping(self):
+    def _ping_callback(self):
         if self._number_of_points == 0:
             self._status.setText('В процессе    ')
             self._number_of_points = 1
